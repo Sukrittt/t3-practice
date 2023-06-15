2408e518106df50bb7b9ee59d3e8a5d7c2ab27b4
Sukrit Saha <sukritsaha27@gmail.com>
2023-06-15 19:38:04 +0530

    creating new pages for post and profile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useUser } from "@clerk/nextjs";
 import Head from "next/head";
 import Link from "next/link";
+import { toast } from "react-hot-toast";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
@@ -13,7 +14,6 @@ import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
 import Image from "next/image";
 import LoadingSpinner, { LoadingPage } from "~/components/loading";
-import { toast } from "react-hot-toast";
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -96,10 +96,14 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-x-2 text-slate-300 ">
-          <span>{`@${author.username}`}</span>
-          <span className="font-thin">{`· ${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{`· ${dayjs(
+              post.createdAt
+            ).fromNow()}`}</span>
+          </Link>
         </div>
 
         <span className="text-2xl">{post.content}</span>
