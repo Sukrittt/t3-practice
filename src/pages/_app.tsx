52379ec47025e0e05316bc4a654f7b712d5ffd8c
Sukrit Saha <sukritsaha27@gmail.com>
2023-06-15 21:19:42 +0530

    developing profile page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,16 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+            <Head>
+        <title>T3 Practice</title>
+        <meta name="description" content="T3 God, okay i'm thinking." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
